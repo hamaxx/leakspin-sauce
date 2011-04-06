04d7015b13a1edbf7155b6431e66482ea471103f
ham <ham@ham-ThinkPad.(none)>
2011-04-07 01:11:28 +0200

    updated tab data

--- graph/getTab.py
+++ graph/getTab.py
@@ -1,19 +1,24 @@
-import os, json
+import os, json, re
 
 #name	created	released	status	source
 
 data = [];
-data.append("name	created	released	status	source")
-data.append("string	c	c	string	string")
-data.append("meta")
+data.append("name	created	released	status	source	title")
+data.append("d	c	c	d	d	d")
+data.append("meta					meta")
 
 def parseFile(fn, dirFrom):
 	f = open(dirFrom + "/" + fn, 'r')
 	cont = []
 	for i in xrange(5):
-		s = f.readline()[0:-1]
+		s = f.readline().decode("utf-8")[0:-1]
 		if i == 1 or i == 2: s = s[0:4]
-		cont.append(s)		
+		cont.append(s)
+	regex = re.compile(r'\nSUBJECT: *(.*?)\n', re.MULTILINE)
+	match = re.search(regex, f.read().decode("utf-8"))
+	if match: cont.append(match.groups()[0])
+	else: cont.append("x")
+	
 	f.close()
 	data.append("\t".join(cont))
 		
@@ -22,7 +27,7 @@ def collectAll(dirFrom):
 	count = 0
 	for i in os.listdir(dirFrom):
 		if os.path.isfile(dirFrom + "/" + i) == True:
-			cons = json.loads(file("../related/" + i).read())
+			cons = json.loads(file("../related/" + i).read().decode("utf-8"))
 			if len(cons) > 0:
 				print count
 				count += 1
@@ -32,5 +37,5 @@ def collectAll(dirFrom):
 collectAll("../filtered")
 
 fileObj = open("./leaks.tab", "w")
-fileObj.write("\n".join(data).encode('utf8'))
+fileObj.write("\n".join(data).encode('ascii', 'replace'))
 fileObj.close()
